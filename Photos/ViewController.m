76d48ac8c36f91f036129f3f1797994559dce334
Sam Meech-Ward <sam@meech-ward.com>
2017-03-04 15:20:24 -0800

    Remove The photo class

--- Photos/ViewController.m
+++ Photos/ViewController.m
@@ -8,12 +8,12 @@
 
 #import "ViewController.h"
 
-#import "Photo.h"
+//#import "Photo.h"
 #import "PhotoCollectionViewCell.h"
 
 @interface ViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
 
-@property (nonatomic, strong) NSArray<Photo *> *photos;
+@property (nonatomic, strong) NSArray<NSDictionary *> *photos;
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
@@ -30,38 +30,31 @@
 
 
 - (void)performRequest {
+    NSURL *url = [NSURL URLWithString:@"https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=ee67416e0ab6d455026b90b4bfb1e5a1&tags=cat&nojsoncallback=1&extras=url_m"];
+    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:url];
     
-    //    NSURL *url = [NSURL URLWithString:@"https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=ee67416e0ab6d455026b90b4bfb1e5a1&has_geo=1&lat=49.282070&lon=123.108296&radius=20"]; // 1
-    
-    NSURL *url = [NSURL URLWithString:@"https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=ee67416e0ab6d455026b90b4bfb1e5a1&tags=cat&nojsoncallback=1"];
-    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:url]; // 2
-    
-    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration]; // 3
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration]; // 4
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
     
     NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         
-        if (error) { // 1
+        if (error) {
             // Handle the error
             NSLog(@"error: %@", error.localizedDescription);
             return;
         }
         
         NSError *jsonError = nil;
-        NSDictionary *jsonFlickrApi = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError]; // 2
+        NSDictionary *jsonFlickrApi = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
         
-        if (jsonError) { // 1
+        if (jsonError) {
             // Handle the error
             NSLog(@"jsonError: %@", jsonError.localizedDescription);
             return;
         }
         
         NSDictionary *photosDictionary = jsonFlickrApi[@"photos"];
-        NSArray *jsonPhotos = photosDictionary[@"photo"]; // 3
-        
-        NSLog(@"photo: %@", jsonPhotos[0]);
-        
-        self.photos = [self createPhotosFromJSON:jsonPhotos];
+        self.photos = photosDictionary[@"photo"];
         
         [[NSOperationQueue mainQueue] addOperationWithBlock:^{
             [self.collectionView reloadData];
@@ -69,21 +62,8 @@
         
     }];
     
-    [dataTask resume]; // 7
+    [dataTask resume];
     
-}
-
-- (NSArray<Photo *> *)createPhotosFromJSON:(NSArray *)jsonPhotos {
-    NSMutableArray<Photo *> *photos = [NSMutableArray array];
-    
-    for (NSDictionary *jsonPhoto in jsonPhotos){
-        
-        Photo *photo = [[Photo alloc] initWithFlickrJSON:jsonPhoto];
-        
-        [photos addObject:photo];
-    }
-    
-    return photos.copy;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -102,87 +82,40 @@
     
     PhotoCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"photo-cell" forIndexPath:indexPath];
     
-    Photo *photo = self.photos[indexPath.row];
-    cell.label.text = photo.title;
+    NSDictionary *photo = self.photos[indexPath.row];
+    cell.label.text = photo[@"title"];
     
-    [self downloadImageForPhoto:photo forPhotoCell:cell];
+    NSString *urlString = photo[@"url_m"];
+    NSURL *url = [NSURL URLWithString:urlString];
+    [self downloadImageAtURL:url andPhotoCell:cell];
     
     return cell;
 }
 
-- (void)downloadImageForPhoto:(Photo *)photo forPhotoCell:(PhotoCollectionViewCell *)cell {
-    
-    if (photo.cachedImage) {
-        cell.imageView.image = photo.cachedImage;
-        return;
-    }
+- (void)downloadImageAtURL:(NSURL *)url andPhotoCell:(PhotoCollectionViewCell *)cell {
     
     NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration]; // 2
     NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration]; // 3
     
-    NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithURL:photo.photoURL completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+    NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithURL:url completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         
-        if (error) { // 1
+        if (error) {
             // Handle the error
             NSLog(@"error: %@", error.localizedDescription);
             return;
         }
         
-        NSData *data = [NSData dataWithContentsOfURL:location]; // 2
+        NSData *data = [NSData dataWithContentsOfURL:location];
         
-        UIImage *image = [UIImage imageWithData:data]; // 3
-        photo.cachedImage = image;
+        UIImage *image = [UIImage imageWithData:data];
         
         [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-            // This will run on the main queue
-            
-            cell.imageView.image = image; // 4
+            cell.imageView.image = image;
         }];
         
     }];
     
-    [downloadTask resume]; // 5
-    
-}
-
-
-- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-    
-    Photo *photo = self.photos[indexPath.row];
-    
-    NSString *photoString = [NSString stringWithFormat:@"https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&format=json&api_key=ee67416e0ab6d455026b90b4bfb1e5a1&tags=cat&nojsoncallback=1&photo_id=%@", photo.flickrId];
-    NSURL *url = [NSURL URLWithString:photoString];
-    NSLog(@"%@", photoString);
-    
-    //    NSURL *url = [NSURL URLWithString:@"https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=ee67416e0ab6d455026b90b4bfb1e5a1&tags=cat&nojsoncallback=1"];
-    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:url]; // 2
-    
-    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration]; // 3
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration]; // 4
-    
-    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        
-        if (error) { // 1
-            // Handle the error
-            NSLog(@"error: %@", error.localizedDescription);
-            return;
-        }
-        
-        NSError *jsonError = nil;
-        NSDictionary *jsonFlickrApi = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError]; // 2
-        
-        if (jsonError) { // 1
-            // Handle the error
-            NSLog(@"jsonError: %@", jsonError.localizedDescription);
-            return;
-        }
-        
-        NSLog(@"flickr: %@", jsonFlickrApi);
-        
-    }];
-    
-    [dataTask resume]; // 7
-    
+    [downloadTask resume];
 }
 
 @end
